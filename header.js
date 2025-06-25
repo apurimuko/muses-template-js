c143bf11246ac111c4800ba894681223e9658bcd
FUJIWARA-aa <206653210+FUJIWARA-aa@users.noreply.github.com>
2025-06-25 14:19:25 +0900

    ヘッダー変更

--- header.js
+++ header.js
@@ -4,7 +4,7 @@ function toggleMenu() {
 }
 
 // ログアウト処理とユーザー名表示
-document.addEventListener("DOMContentLoaded", () => {
+const loadHeaderData = () => {
   // ユーザー名の表示処理
   const username = sessionStorage.getItem("username");
 
@@ -23,22 +23,22 @@ document.addEventListener("DOMContentLoaded", () => {
     sessionStorage.removeItem("username"); // セッション情報削除
     location.href = "login copy.html"; // ログインページに戻る
   });
-});
 
-// 残り時間のタイマー表示
-let remainingSeconds = 2400; // 40分
+  // 残り時間のタイマー表示
+  let remainingSeconds = 2400; // 40分
 
-function updateTime() {
-  const minutes = Math.floor(remainingSeconds / 60);
-  const seconds = remainingSeconds % 60;
-  document.getElementById("clock").textContent = `${minutes}:${seconds
-    .toString()
-    .padStart(2, "0")}`;
+  function updateTime() {
+    const minutes = Math.floor(remainingSeconds / 60);
+    const seconds = remainingSeconds % 60;
+    document.getElementById("clock").textContent = `${minutes}:${seconds
+      .toString()
+      .padStart(2, "0")}`;
 
-  if (remainingSeconds > 0) {
-    remainingSeconds--;
-    setTimeout(updateTime, 1000);
+    if (remainingSeconds > 0) {
+      remainingSeconds--;
+      setTimeout(updateTime, 1000);
+    }
   }
-}
 
-updateTime();
+  updateTime();
+};
